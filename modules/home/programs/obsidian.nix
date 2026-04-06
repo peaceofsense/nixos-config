d0d557f5e46d991919bfacc5336642d6f80f62d2
peaceofsense <72244031+peaceofsense@users.noreply.github.com>
2026-04-06 23:48:35 +0200

    Updated obsidian vault source

--- modules/home/programs/obsidian.nix
+++ modules/home/programs/obsidian.nix
@@ -4,7 +4,7 @@ let
     #!/bin/sh
 
     # Set the vault directory
-    VAULT_DIR="$HOME/my-files/vault"
+    VAULT_DIR="$HOME/my-files/TH25"
 
     # Change to the vault directory, exit if it doesn't exist
     cd "$VAULT_DIR" || { echo "Error: Vault directory $VAULT_DIR not found." >&2; exit 1; }
